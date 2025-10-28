3547275ce363a8e6b8e1b232ad93deeb6235d569
“Habibamohamedux” <Habibasaad005@outlokk.com>
2025-10-28 12:08:58 +0300

    editi

---
+++ src/components/layout/Herohome.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './Herohome.css'
+const Herohome = () => {
+    return ( 
+        <div className="herohome-container">
+            <div className="herohome-content">
+                <h1 className="herohome-title">Welcome to My Portfolio</h1>
+                <p className="herohome-subtitle">Showcasing My Projects and Skills</p>
+            </div>
+        </div>
+     );
+}
+ 
+export default Herohome;
