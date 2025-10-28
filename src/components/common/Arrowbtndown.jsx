afaacae5426edfa9a319c9c258d89b6bc7138d6b
“Habibamohamedux” <Habibasaad005@outlokk.com>
2025-10-28 17:06:48 +0300

    hero2

--- src/components/common/Arrowbtndown.jsx
+++ src/components/common/Arrowbtndown.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
-import '../layout/Herohome.css';
+import './Herohome.css';
 import Textgenration from '../common/Textgenration.jsx';
 import Video from "../../assets/motiongraphics/HabibaSaad_MOTIONGRAPHICS_Y3_SHOWREEL01_001.mp4";
 import Arrowbtndown from '../common/Arrowbtndown.jsx';
+
 const Herohome = () => {
   return (
-    <>
     <div className="herohome-container">
       <video className="herohome-video" src={Video} autoPlay loop muted playsInline></video>
+
       <div className="herohome-overlay">
         <Textgenration />
       </div>
 
+      {/* Arrow button above video, positioned absolutely */}
+      <Arrowbtndown />
     </div>
-    <Arrowbtndown />
-           </>
   );
 };
 
