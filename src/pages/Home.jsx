4b0b7d4c4acc8b8841be620a4cd2f4f4b5d1484b
“Habibamohamedux” <Habibasaad005@outlokk.com>
2025-10-28 12:08:58 +0300

    editi

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./Home.css";
+import Herohome from "../components/layout/Herohome.jsx";
 import Navbar from "../components/layout/Navbar.jsx";
 const Home = () => {
     return (
         <>
 
             <Navbar />
+            <Herohome />
+
         </>
       );
 }
